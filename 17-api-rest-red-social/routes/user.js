b512dfc7d7016746da62c9c464c7625ff588dc7d
deivy <developerdeivymorales@gmail.com>
2024-01-23 16:42:18 -0500

    :sparkles: Serializar formulario de editar usuario.

--- 17-api-rest-red-social/routes/user.js
+++ 17-api-rest-red-social/routes/user.js
@@ -29,7 +29,7 @@ router.post(
   [check.auth, uploads.single("file0")],
   UserController.upload
 );
-router.get("/avatar/:file", check.auth, UserController.avatar);
+router.get("/avatar/:file", UserController.avatar);
 router.get("/counters/:id", check.auth, UserController.counters);
 
 // Exportar router
--- 17-proyectoApiSocial/src/components/user/Config.jsx
+++ 17-proyectoApiSocial/src/components/user/Config.jsx
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
+import useAuth from "../../hooks/useAuth";
+import { Global } from "../../helpers/Global";
+import { SerializeForm } from "../../helpers/SerializeForm";
 
 export const Config = () => {
+  const { auth } = useAuth();
+
   const [saved, setSaved] = useState("not_sended");
 
   const updateUser = (e) => {
     e.preventDefault();
+    // Recoger datos del formulario
+    let newDataUser = SerializeForm(e.target);
+    // Borrar propiedad innecesaria
+    delete newDataUser.file0;
+
+    // Actualizar usuario en la base de datos
+    
   };
   return (
     <>
@@ -31,22 +43,27 @@ export const Config = () => {
         <form className="config-form" onSubmit={updateUser}>
           <div className="form-group">
             <label htmlFor="name">Name</label>
-            <input type="text" name="name" />
+            <input type="text" name="name" defaultValue={auth.name} />
           </div>
 
           <div className="form-group">
             <label htmlFor="surname">Apellidos</label>
-            <input type="text" name="surname" />
+            <input type="text" name="surname" defaultValue={auth.surname} />
           </div>
 
           <div className="form-group">
             <label htmlFor="nick">Nick</label>
-            <input type="text" name="nick" />
+            <input type="text" name="nick" defaultValue={auth.nick} />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="bio">Bio</label>
+            <textarea name="bio" defaultValue={auth.bio} />
           </div>
 
           <div className="form-group">
             <label htmlFor="email">Correo electronico</label>
-            <input type="email" name="email" />
+            <input type="email" name="email" defaultValue={auth.email} />
           </div>
 
           <div className="form-group">
@@ -56,7 +73,23 @@ export const Config = () => {
 
           <div className="form-group">
             <label htmlFor="file0">Avatar</label>
-            <div className="avatar">{/* Mostrar imagen */}</div>
+            <div className="general-info__container-avatar">
+              {auth.image != "default.png" && (
+                <img
+                  src={Global.url + "user/avatar/" + auth.image}
+                  className="container-avatar__img"
+                  alt="Foto de perfil"
+                />
+              )}
+              {auth.image == "default.png" && (
+                <img
+                  src={avatar}
+                  className="container-avatar__img"
+                  alt="Foto de perfil"
+                />
+              )}
+            </div>
+            <br />
             <input type="file" name="file0" id="file" />
           </div>
           <br />
