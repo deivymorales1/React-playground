aa2fed115f61e5014092a7e7a017ed04f97c84f6
deivy <developerdeivymorales@gmail.com>
2024-01-31 13:21:37 -0500

    :hammer: Abstraccion de publicaciones

--- 17-proyectoApiSocial/src/components/user/Profile.jsx
+++ 17-proyectoApiSocial/src/components/user/Profile.jsx
@@ -4,6 +4,7 @@ import { GetProfile } from "../../helpers/GetProfile";
 import { Link, useParams } from "react-router-dom";
 import { Global } from "../../helpers/Global";
 import useAuth from "../../hooks/useAuth";
+import { PublicationList } from "../publication/PublicationList";
 
 const Profile = () => {
   const { auth } = useAuth();
@@ -128,33 +129,6 @@ const Profile = () => {
     }
   };
 
-  // Metodo para paginar
-  const nextPage = () => {
-    let next = page + 1;
-    setPage(next);
-    getPublications(next);
-  };
-
-  // Metodo eliminar publication
-  const deletePublication = async (publicationId) => {
-    const request = await fetch(
-      Global.url + "publication/remove/" + publicationId,
-      {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: localStorage.getItem("token"),
-        },
-      }
-    );
-
-    const data = await request.json();
-
-    setPage(1);
-    setMore(true);
-    getPublications(1, true);
-  };
-
   return (
     <>
       <header className="aside__profile-info">
@@ -241,81 +215,15 @@ const Profile = () => {
         </div>
       </header>
 
-      {publications.map((publication) => {
-        return (
-          <article className="content__posts" key={publication.id}>
-            <div className="posts__post">
-              <div className="post__container">
-                <div className="post__image-user">
-                  <a href="#" className="post__image-link">
-                    <Link
-                      to={"/social/perfil/" + publication.user._id}
-                      className="post__image-link"
-                    >
-                      {publication.user.image != "default.png" && (
-                        <img
-                          src={
-                            Global.url + "user/avatar/" + publication.user.image
-                          }
-                          className="post__user-image"
-                          alt="Foto de perfil"
-                        />
-                      )}
-                      {publication.user.image == "default.png" && (
-                        <img
-                          src={avatar}
-                          className="post__user-image"
-                          alt="Foto de perfil"
-                        />
-                      )}
-                    </Link>
-                  </a>
-                </div>
+      <PublicationList
+        publications={publications}
+        getPublications={getPublications}
+        page={page}
+        setPage={setPage}
+        more={more}
+        setMore={setMore}
+      />
 
-                <div className="post__body">
-                  <div className="post__user-info">
-                    <a href="#" className="user-info__name">
-                      {publication.user.name + " " + publication.user.surname}
-                    </a>
-                    <span className="user-info__divider"> | </span>
-                    <a href="#" className="user-info__create-date">
-                      {publication.created_at}
-                    </a>
-                  </div>
-
-                  <h4 className="post__content"> {publication.text} </h4>
-                  {publication.file && (
-                    <img
-                      src={
-                        Global.url + "publication/media/" + publication.files
-                      }
-                    />
-                  )}
-                </div>
-              </div>
-
-              {auth._id == publication.user._id && (
-                <div className="post__buttons">
-                  <button
-                    onClick={() => deletePublication(publication._id)}
-                    className="post__button"
-                  >
-                    <i className="fa-solid fa-trash-can"></i>
-                  </button>
-                </div>
-              )}
-            </div>
-          </article>
-        );
-      })}
-
-      {more && (
-        <div className="content__container-btn">
-          <button className="content__btn-more-post" onClick={nextPage}>
-            Ver mas publicaciones
-          </button>
-        </div>
-      )}
       <br />
     </>
   );
