25f3712f0f2db60fb2080b3060352c847004bfd2
deivy <130388892+deivymorales1@users.noreply.github.com>
2024-03-04 22:02:28 -0500

    :sparkles: Formulario con React y Formik

--- 18-mas-forms/src/components/MiFormulario.jsx
+++ 18-mas-forms/src/components/MiFormulario.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { useFormik } from "formik";
 
 const MiFormulario = () => {
-  return <div>MiFormulario</div>;
+  const formik = useFormik({
+    initialValues: {
+      nombre: "deivy",
+      email: "deivyrocha@gmail.com",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  return (
+    <div>
+      <h1>Mi formulario con formik</h1>
+      <form onSubmit={formik.handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="nombre">Nombre</label>
+          <input
+            type="text"
+            id="nombre"
+            name="nombre"
+            value={formik.values.nombre}
+            onChange={formik.handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+          />
+        </div>
+        <input type="submit" />
+      </form>
+    </div>
+  );
 };
 
 export default MiFormulario;
