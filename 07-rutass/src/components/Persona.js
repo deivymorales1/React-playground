9ca32f3d3698bd22e3d570a5dd4f83e4a1d251bb
deivy <130388892+deivymorales1@users.noreply.github.com>
2024-03-04 20:47:25 -0500

    :alembic: useNavigate

--- 07-rutass/src/components/Persona.js
+++ 07-rutass/src/components/Persona.js
@@ -1,8 +1,24 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Persona = () => {
   let { nombre, apellido } = useParams();
+  const navegar = useNavigate();
+
+  const enviar = (e) => {
+    e.preventDefault();
+    let nombre = e.target.nombre.value;
+    let apellido = e.target.apellido.value;
+    let url = `/persona/${nombre}/${apellido}`;
+
+    if (nombre.length <= 0 && apellido.length <= 0) {
+      navegar("/inicio");
+    } else if (nombre === "contacto") {
+      navegar("/contacto");
+    } else {
+      navegar(url);
+    }
+  };
 
   return (
     <div>
@@ -13,6 +29,12 @@ const Persona = () => {
         </h1>
       )}
       <p>Esta es la pagina de persona</p>
+
+      <form onSubmit={enviar}>
+        <input type="text" name="nombre" />
+        <input type="text" name="apellido" />
+        <input type="submit" name="enviar" value="enviar" />
+      </form>
     </div>
   );
 };
